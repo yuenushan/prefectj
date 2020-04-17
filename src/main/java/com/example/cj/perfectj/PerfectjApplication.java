91a41af4b80b6920830bda499d1b2aea32209643
陈京 <chenjing05@kuaishou.com>
2020-04-17 23:35:07 +0800

    init

---
+++ src/main/java/com/example/cj/perfectj/PerfectjApplication.java
@@ -0,0 +1,13 @@
+package com.example.cj.perfectj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfectjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cj/perfectj/PerfectjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cj.perfectj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
