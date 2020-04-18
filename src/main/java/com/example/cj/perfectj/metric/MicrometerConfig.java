814eabfc453063a910ff1724a5c53b751b833065
陈京 <chenjing05@kuaishou.com>
2020-04-18 11:17:46 +0800

    prometheus监控

---
+++ src/main/java/com/example/cj/perfectj/metric/MicrometerConfig.java
@@ -0,0 +1,24 @@
+package com.example.cj.perfectj.metric;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.core.instrument.MeterRegistry;
+
+/**
+ * Created on 2020-04-18
+ */
+@Configuration
+public class MicrometerConfig {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+
+}
