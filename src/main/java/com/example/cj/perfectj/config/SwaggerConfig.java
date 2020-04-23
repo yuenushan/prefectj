d24bd12750bf8bb4d8e8a6075c6017e641177479
陈京 <chenjing05@kuaishou.com>
2020-04-23 11:02:00 +0800

    添加swagger

---
+++ src/main/java/com/example/cj/perfectj/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.example.cj.perfectj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created on 2020-04-23
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.example.cj.perfectj.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        //        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+    }
+
+    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot 使用 Swagger2 构建RESTful API")
+                .contact(new Contact("david", "", "425800970@qq.com"))
+                .version("1.0")
+                .description("API 描述")
+                .build();
+    }
+}
--- src/main/java/com/example/cj/perfectj/interceptor/MvcInterceptorConfig.java
+++ src/main/java/com/example/cj/perfectj/interceptor/MvcInterceptorConfig.java
@@ -3,6 +3,7 @@ package com.example.cj.perfectj.interceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
@@ -18,5 +19,16 @@ public class MvcInterceptorConfig extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(apiMonitorInterceptor).addPathPatterns("/*");
         super.addInterceptors(registry);
+    }
+
+    // TODO 待研究
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 }
