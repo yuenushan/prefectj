e1b8763d4a1fb33deec2bf1d813740f092d99190
陈京 <chenjing05@kuaishou.com>
2020-04-25 23:51:44 +0800

    引入一个自己实现的stater demo

--- src/main/java/com/example/cj/perfectj/controller/HandsomeController.java
+++ src/main/java/com/example/cj/perfectj/controller/HandsomeController.java
@@ -3,21 +3,26 @@ package com.example.cj.perfectj.controller;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cj.perfectj.tool.ResponseUtil;
 import com.example.cj.perfectj.tool.ResponseUtil.Response;
+import com.example.cj.perfectstarter.HandsomeService;
 
 /**
  * Created on 2020-04-17
  */
 @RestController
-public class AwesomeController {
+public class HandsomeController {
 
-    @GetMapping("/awesome")
-    public Response awesome() {
-        return ResponseUtil.buildSuccess("Don't ask, ask just test you awesome!");
+    @Autowired
+    private HandsomeService handsomeService;
+
+    @GetMapping("/handsome")
+    public Response handsome() {
+        return ResponseUtil.buildSuccess(handsomeService.getInfo());
     }
 
     @GetMapping("")
