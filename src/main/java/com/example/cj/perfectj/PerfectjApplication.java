4d8097d9a6cda34294546f92ef65d9a7c22fbeda
陈京 <chenjing05@kuaishou.com>
2020-06-09 20:30:45 +0800

    add springboot close log

--- src/main/java/com/example/cj/perfectj/PerfectjApplication.java
+++ src/main/java/com/example/cj/perfectj/PerfectjApplication.java
@@ -1,6 +1,9 @@
 package com.example.cj.perfectj;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,10 +12,16 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 // 通过直接加@Mapper注解了。发现在写测试用例的时候，也会去扫描mapper，导致mock的时候出错
 //@MapperScan("com.example.cj.perfectj.mapper")
-public class PerfectjApplication {
+public class PerfectjApplication implements DisposableBean {
+
+    private final Logger logger = LoggerFactory.getLogger(PerfectjApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(PerfectjApplication.class, args);
     }
 
+    @Override
+    public void destroy() {
+        logger.info("application shutdown.");
+    }
 }
