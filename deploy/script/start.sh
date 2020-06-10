a8471b15d8bddf9a1296521f3ac3992a3e2c6c58
陈京 <chenjing05@kuaishou.com>
2020-06-10 17:30:10 +0800

    jvm参数添加

--- deploy/script/start.sh
+++ deploy/script/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profile.active=prod \
+-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap \
+-XX:+UseG1GC -Xmx1500m -Xms1500m -XX:MaxGCPauseMillis=200 \
 -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -Xloggc:log/gc-%t.log \
 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/log -jar /app.jar
